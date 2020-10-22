64229124b5746e95bca5cc4605f125e77d9e46e7
mercspring <68706508+mercspring@users.noreply.github.com>
2020-10-21 21:00:47 -0700

    added routes to get types, breeds, get dogs by zip code, update a pet, add a pet and destroy a pet

--- routes/routes.js
+++ routes/routes.js
@@ -11,78 +11,92 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-        db.User.create({
-            first_name: "Hack",
-            last_name: "Saw",
-            email: "hack@saw.com",
-            zip:98204
-
-
-        })
-        
-        db.type.create({
-            type: "Dog"
-        })
-        db.breed.create({
-            breed: "German Shepard",
-            typeId: 1,
-        })
-        db.Pet.create({
-            name: "Dog",
-            gender: "male",
-            age: 7,
-            zip: 98204,
-            breedId: 1,
-            color: "black",
-            vacinated: true,
-            hobbies: "barking at the neighbors dogs",
-            UserId: 1
-        })
-        res.send()
-    });
     // GET /api/user/:user_id
     // GET /api/user/:user_id/pet
     // GET /api/type/
-    app.get("/api/type", function(req, res) {
-        db.petType.findAll().then(function(dbTypes) {
+    app.get("/api/type", function (req, res) {
+        db.type.findAll().then(function (dbTypes) {
             res.json(dbTypes);
-          });
-      });
+        });
+    });
     // GET /api/breed/
-    app.get("/api/breed", function(req, res) {
-        db.petType.findAll().then(function(dbBreeds) {
+    app.get("/api/breed", function (req, res) {
+        db.breed.findAll().then(function (dbBreeds) {
             res.json(dbBreeds);
-          });
-      })
+        });
+    })
     /*******************************/
     // GET /api/zip/:zip
-    app.get("/api/zip/:zip", function(req, res) {
+    app.get("/api/zip/:zip", function (req, res) {
         db.User.findAll({
-            attributes: [],
+            attributes: { exclude: ['createdAt', 'updatedAt'] },
             include: [{
+                attributes: { exclude: ['createdAt', 'updatedAt'] },
                 model: db.Pet,
-                attributes: [['name', 'age']]
-              },
+                include: {
+                    attributes: { exclude: ['createdAt', 'updatedAt'] },
+                    model: db.breed,
+                    include: {
+                        attributes: { exclude: ['createdAt', 'updatedAt'] },
+                        model: db.type
+                    }
+                }
+            },
             ],
             where: {
-              zip: req.params.zip
+                zip: req.params.zip
             }
-          }).then(result => {
-              res.send(result)
-          })
-      })
+        }).then(result => {
+            res.send(result)
+        })
+    })
     // OR 
     // GET /api/:zip/:radius 
     /*******************************/
 
     // PUT api/user/:user_id
     // PUT api/pet/:pet_id
+    app.put("/api/pet/:pet_id", function (req, res) {
+        db.Pet.update({
+            name: req.body.name,
+            gender: req.body.gender,
+            age: req.body.age,
+            color: req.body.color,
+            vacinated: req.body.vacinated,
+            hobbies: req.body.hobbies,
+            breedId: req.body.breedId,
+        },{
+            where: {id: req.params.pet_id}
+        }).then(function (pet) {
+            res.send("it worked");
+        });
+    }) 
 
     // DELETE api/user/:user_id
     // DELETE api/pet/:pet_id
-
+    app.delete("/api/pet/:pet_id", function (req, res) {
+        db.Pet.destroy({
+            where:{
+                id: req.params.pet_id
+            }
+        }).then(function (pet) {
+            res.send("it worked");
+        });
+    }) 
     // POST api/user/
     // POST api/pet/
-
+    app.post("/api/pet/", function (req, res) {
+        db.Pet.create({
+            name: req.body.name,
+            gender: req.body.gender,
+            age: req.body.age,
+            color: req.body.color,
+            vacinated: req.body.vacinated,
+            hobbies: req.body.hobbies,
+            breedId: req.body.breedId,
+            userId: req.body.userId
+        }).then(function (pet) {
+            res.send("it worked");
+        });
+    }) 
 };
