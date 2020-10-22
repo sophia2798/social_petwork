6cf9171430df0802a7273c462cbdf362a269d013
Katie Dickson <katieedicksonn1@gmail.com>
2020-10-22 10:38:33 -0700

    petwork page

---
+++ views/public/assets/script.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function(){
+    $('.carousel').carousel({
+        numVisable: 6,
+        shift: 55,
+        padding: 55,
+    });
+  });
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+    
+  });
+
