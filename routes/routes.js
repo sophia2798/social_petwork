7ebc9e862f6644ccc6581c172b90186e5f199a3d
mercspring <68706508+mercspring@users.noreply.github.com>
2020-10-21 19:50:59 -0700

    partial commit

--- routes/routes.js
+++ routes/routes.js
@@ -11,16 +11,69 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function (app) {
+    app.get("/", function (req, res) {
+        db.User.create({
+            first_name: "Hack",
+            last_name: "Saw",
+            email: "hack@saw.com",
+            zip:98204
 
+
+        })
+        
+        db.type.create({
+            type: "Dog"
+        })
+        db.breed.create({
+            breed: "German Shepard",
+            typeId: 1,
+        })
+        db.Pet.create({
+            name: "Dog",
+            gender: "male",
+            age: 7,
+            zip: 98204,
+            breedId: 1,
+            color: "black",
+            vacinated: true,
+            hobbies: "barking at the neighbors dogs",
+            UserId: 1
+        })
+        res.send()
+    });
     // GET /api/user/:user_id
     // GET /api/user/:user_id/pet
     // GET /api/type/
+    app.get("/api/type", function(req, res) {
+        db.petType.findAll().then(function(dbTypes) {
+            res.json(dbTypes);
+          });
+      });
     // GET /api/breed/
-
+    app.get("/api/breed", function(req, res) {
+        db.petType.findAll().then(function(dbBreeds) {
+            res.json(dbBreeds);
+          });
+      })
     /*******************************/
-    // /api/pet/zip
+    // GET /api/zip/:zip
+    app.get("/api/zip/:zip", function(req, res) {
+        db.User.findAll({
+            attributes: [],
+            include: [{
+                model: db.Pet,
+                attributes: [['name', 'age']]
+              },
+            ],
+            where: {
+              zip: req.params.zip
+            }
+          }).then(result => {
+              res.send(result)
+          })
+      })
     // OR 
-    // /api/:zip/:radius 
+    // GET /api/:zip/:radius 
     /*******************************/
 
     // PUT api/user/:user_id
